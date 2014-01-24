7f0c75599cfe4a88bddc3c39ee0689043daf1fa5
Matt Bertino <mattbertino@gmail.com>
2014-01-24 15:34:07 -0500

    -n build commit 1/24/2014

---
+++ scripts/52f824e5.main.js
@@ -0,0 +1,1 @@
+$(".screenshot").hover(function(){$(this).children(".caption").toggle("slow")}),$(".screenshot").click(function(){console.log("this worked"),$(".popupbox").toggle("slow"),$(".transsheet").toggle("slow")}),$(".popupboxclose").click(function(){console.log("this worked"),$(".popupbox").toggle("slow"),$(".transsheet").toggle("slow")});
